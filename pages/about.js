0f3e7184f9a9b12ea04d065599c472f86d9073ad
Caleb Kandoro <kandoroc@hotmail.com>
2022-12-05 09:52:49 +0200

    crossOrigin bug fix

--- pages/about.js
+++ pages/about.js
@@ -11,7 +11,7 @@ export default function About() {
   return <>
     <Head>
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
       <link href="https://fonts.googleapis.com/css2?family=Tangerine:wght@700&display=swap" rel="stylesheet" />
     </Head>
     <main>
